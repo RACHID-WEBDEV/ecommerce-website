cae18898bfa39715cbd0f67904c4001eb214ca5f
RACHID-WEBDEV <rasheedolamide2015@gmail.com>
2021-06-19 07:41:25 +0100

    ecommerce commit

---
+++ app/Http/Livewire/Admin/AdminHomeSliderComponent.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Livewire\Admin;
+
+use App\Models\HomeSlider;
+use Livewire\Component;
+
+class AdminHomeSliderComponent extends Component
+{
+
+    public function deleteSlide($slide_id)
+    {
+        $slider = HomeSlider::find($slide_id);
+        $slider->delete();
+        Session()->flash('message', 'Slider has been Deleted Successfully');
+    }
+    public function render()
+    {
+       $sliders = HomeSlider::all();
+
+        return view('livewire.admin.admin-home-slider-component',['sliders'=>$sliders])->layout('layouts.base');
+    }
+}
---
+++ app/Http/Livewire/Admin/AdminProductComponent.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Livewire\Admin;
+
+use App\Models\Product;
+use Livewire\WithPagination;
+use Livewire\Component;
+
+class AdminProductComponent extends Component
+{
+    use WithPagination;
+
+    public function deleteProduct($id)
+    {
+        $product = Product::find($id);
+        $product->delete();
+        Session()->flash('message', 'Product has been Deleted Successfully');
+    }
+    public function render()
+    {
+        $products = Product::paginate(10); 
+        return view('livewire.admin.admin-product-component',['products'=>$products])->layout('layouts.base');
+    }
+}
